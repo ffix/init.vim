2e7650d328475451253de0c2bbbd1577d0f07eb9
Sergey Ninua <dev@ffix.me>
2022-08-10 22:20:19 +0300

    Remove unneeded plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -15,9 +15,6 @@ vim.cmd [[
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
-  -- use 'tpope/vim-fugitive' -- Git commands in nvim
-  -- use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
-  -- use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
   use {
     'numToStr/Comment.nvim',
     config = function()
@@ -27,9 +24,7 @@ require('packer').startup(function()
   use 'ludovicchabant/vim-gutentags' -- Automatic tags management
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  -- use 'joshdick/onedark.vim' -- Theme inspired by Atom
   use { 'ellisonleao/gruvbox.nvim', requires = { 'rktjmp/lush.nvim' } } -- gruvbox theme
-  -- use 'itchyny/lightline.vim' -- Fancier statusline
   use {
     'hoob3rt/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
@@ -46,23 +41,14 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
   -- use 'kabouzeid/nvim-lspinstall' -- lspinstall
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  -- use 'hrsh7th/cmp-buffer'
-  -- use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
   use 'rafamadriz/friendly-snippets'
   use 'RRethy/vim-illuminate' -- Illuminate
-  -- neovim tree
   use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
-  -- use 'lyokha/vim-xkbswitch'
-  use 'Pocco81/NoCLC.nvim'
   use 'ahmedkhalf/project.nvim'
-  -- use 'tpope/vim-surround'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-repeat'
   use 'windwp/nvim-autopairs'
-  -- use 'svermeulen/vim-cutlass'
   use 'famiu/bufdelete.nvim'
   use {
     'phaazon/hop.nvim',
